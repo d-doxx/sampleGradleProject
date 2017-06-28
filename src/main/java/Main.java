002d2f0ed01ccf8675aa41b15cf768becbc951d1
root <root@localhost.localdomain>
2017-06-28 15:48:48 +0530

    tmp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Yo what's up human! Oh you think you're fancy");
+		System.out.println("Testing for Gradle Integration with Jenkins.\nSuccess\n");
 	}
 }
